4ccd02a8bb321241c94d820f449b17f2bf24ef93
Ankur Agarwal <ankur@acceldata.io>
2025-07-03 11:46:42 +0530

    remove preview
    remove supabase integration icon

--- app/components/chat/ChatBox.tsx
+++ app/components/chat/ChatBox.tsx
@@ -11,8 +11,6 @@ import { SendButton } from './SendButton.client';
 import { IconButton } from '~/components/ui/IconButton';
 import { toast } from 'react-toastify';
 import { SpeechRecognitionButton } from '~/components/chat/SpeechRecognition';
-import { SupabaseConnection } from './SupabaseConnection';
-import { ExpoQrModal } from '~/components/workbench/ExpoQrModal';
 import styles from './BaseChat.module.scss';
 import type { ProviderInfo } from '~/types/model';
 import { ColorSchemeDialog } from '~/components/ui/ColorSchemeDialog';
@@ -323,8 +321,8 @@ export const ChatBox: React.FC<ChatBoxProps> = (props) => {
               <kbd className="kdb px-1.5 py-0.5 rounded bg-bolt-elements-background-depth-2">Return</kbd> a new line
             </div>
           ) : null}
-          <SupabaseConnection />
-          <ExpoQrModal open={props.qrModalOpen} onClose={() => props.setQrModalOpen(false)} />
+          {/* <SupabaseConnection /> */}
+          {/* <ExpoQrModal open={props.qrModalOpen} onClose={() => props.setQrModalOpen(false)} /> */}
         </div>
       </div>
     </div>
